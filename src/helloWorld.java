b2dc8c135f6982efe23e281931e4abd2f960c77c
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-03 11:27:15 -0500

    complete part 1 and 2 of exercise 2

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,8 +1,17 @@
 
 public class helloWorld {
     public static void main(String[] args) {
+        // Exercise 1
         System.out.println("Hello, World!");
         System.out.println("Helloooooo, World!");
+
+        // Exercise 2
+        int myFavoriteNumber = 48;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "This is my string! leave it alone!";
+        System.out.println(myString);
+
 
     }
 }
