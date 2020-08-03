778d3b34864a8df542e4f452a757ada72ad7e83d
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-03 15:41:21 -0500

    user interface console room area calculator

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -16,20 +16,24 @@ public class ConsoleExercises {
         System.out.println("You entered the number: " + number);
 
         System.out.println("Please enter 3 words");
-
-
         String word1 = scanner.next();
         String word2 = scanner.next();
         String word3 = scanner.next();
-
         System.out.println("Thank you!");
         System.out.printf("you entered the words: %s, %s, and %s.\n", word1, word2, word3);
 
-        System.out.println("Please enter a sentance.");
-
+        System.out.println("Please enter a sentance.\n");
         String sentence = scanner.next();
+        System.out.printf("The sentance you have entered is: %s\n\n", sentence);
 
-        System.out.printf("The sentance you have entered is: %s\n", sentence);
+        System.out.println("Let's figure out the area of the room you're in!\n");
+        System.out.print("About what is the width of your room in feet? : ");
+        int width = scanner.nextInt();
+        System.out.print("About what is the legnth of your room in feet? : ");
+        int length = scanner.nextInt();
+        int area = length * width;
+
+        System.out.println("The area of your room is about " + area + " square feet!\n");
 
     }
 }
