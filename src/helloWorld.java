e11a53a34911cfccef4f5ddde1a124f755eca3f7
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-03 12:15:41 -0500

    complete 2nd half of exercise 2 basic math in Java

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -12,6 +12,28 @@ public class helloWorld {
         String myString = "This is my string! leave it alone!";
         System.out.println(myString);
 
+        Long myNumber = 123L;
+        System.out.println(myNumber);
 
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+
+        int y = 5;
+        System.out.println(++y);
+        System.out.println(y);
+
+        int z = 4;
+        z += 5;
+        System.out.println(z);
+
+        int a = 3;
+        int b = 4;
+        b *= a;
+        System.out.println(b);
+
+        x = Integer.MAX_VALUE;
+        x++;
+        System.out.println("MAXVAL + 1 " + x);
     }
 }
