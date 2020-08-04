04a11b1268c4a89d3d8da6d294677a36be1f3030
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-04 18:55:11 -0500

    created a if else to talk with bob and get different responses depending on the context

---
+++ src/bob.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class bob {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in).useDelimiter("\n");
+
+        System.out.println("Bob is is a lackadaisical teenager\nSay something to Bob or ask him a question!");
+
+        String statement = input.nextLine();
+
+        if(statement.endsWith("?")){
+            System.out.println("Bob: Sure");
+        }else if(statement.endsWith("!")){
+            System.out.println("Bob: Whoa, chill out!");
+        }else if(statement.isEmpty()){
+            System.out.println("Bob: Fine. Be that way!");
+        }else{
+            System.out.println("Bob: Whatever.");
+        }
+        
+    }
+}
