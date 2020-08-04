1debe07208f91bcb1f03bc3d6f426305fe1840e9
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-04 15:48:14 -0500

    practice with strings in java

---
+++ src/StringExercise.java
@@ -0,0 +1,13 @@
+public class StringExercise {
+    public static void main(String[] args) {
+        String floyd = "We don't need no education\nWe don't need no thought control";
+        System.out.println(floyd);
+        String quote ="Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(quote);
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+        String showoff = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(showoff);
+    }
+}
