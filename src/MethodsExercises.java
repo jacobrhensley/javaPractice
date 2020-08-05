b6a14ad7a974694c7243731c20a981763c1e04bc
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-04 23:21:37 -0500

    refactor multiplication into recursion method

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -3,6 +3,7 @@ public class MethodsExercises {
         System.out.println(addition(5, 5));
         System.out.println(subtraction(5, 2));
         System.out.println(multiplication(5, 5));
+        System.out.println(multiplyRecursion(6, 3));
         System.out.println(division(25, 5));
         System.out.println(modulus(10, 2));
         System.out.println(modulus(11, 2));
@@ -27,4 +28,15 @@ public class MethodsExercises {
     public static Integer modulus(int param1, int param2){
         return param1 % param2;
         }
+
+    public static int multiplyRecursion(int num1, int num2) {
+        if (num1 == 0 || num2 == 0) {
+            return 0;
+        }
+        
+        else {
+           return num1 + multiplyRecursion(num1, num2 - 1);
+        }
+        
+    }
 }
