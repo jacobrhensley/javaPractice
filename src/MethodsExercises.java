3d777f460cf41bad409a639feec3bb2c03237f1e
Jacob Hensley <jacobrhensley3@gmail.com>
2020-08-04 23:37:27 -0500

    create recursion method to continue prompting till given a number within called range

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MethodsExercises {
     public static void main(String[] args) {
         System.out.println(addition(5, 5));
@@ -7,6 +9,7 @@ public class MethodsExercises {
         System.out.println(division(25, 5));
         System.out.println(modulus(10, 2));
         System.out.println(modulus(11, 2));
+        System.out.println(getInteger(0, 10));
 
 
     }
@@ -38,5 +41,21 @@ public class MethodsExercises {
            return num1 + multiplyRecursion(num1, num2 - 1);
         }
         
+    }
+    
+    public static int getInteger(int min, int max) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number between 1 and 10: ");
+        int userInput = Integer.parseInt(scan.nextLine());
+
+        // how to proceed
+        if( userInput < min || userInput > max ){
+            System.out.println("Number is outside of the range");
+            return getInteger(min, max);
+        } else {
+            // how to stop
+            System.out.println("Number is within the range");
+            return userInput;
+        }
     }
 }
