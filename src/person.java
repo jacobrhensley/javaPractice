477aebc44021a467044c91135ed5a281451977e6
Jacob Hensley <jacobrhensley3@gmail.com>
2020-10-02 10:46:49 -0500

    experimented with object comparisons when creating the object delaring it equals another or creating one by itself with the same values as another

--- src/person.java
+++ src/person.java
@@ -6,6 +6,24 @@ class Person {
         Jacob.setName("Jacob");
 
         Jacob.sayHello();
+
+            // Person person1 = new Person();
+            // person1.setName("John");
+
+            // Person person2 = person1;
+            // System.out.println(person1 == person2);
+
+
+            // System.out.println(person1.getName());
+            // System.out.println(person2.getName());
+            // person2.setName("Jane");
+            // System.out.println(person1.getName());
+            // System.out.println(person2.getName());
+
+            // Person person2 = new Person();
+            // person2.setName("John");
+            // System.out.println(person1.getName().equals(person2.getName()));
+            // System.out.println(person1 == person2);
     }
 
 
